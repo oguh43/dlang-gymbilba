0fdf7162220e62b7d7fbad98802b270950bf7470
oguh43 <agenthugo43@gmail.com>
2020-12-07 21:06:21 +0100

    Update app.d

--- app.d
+++ app.d
@@ -7,6 +7,8 @@ import std.algorithm.searching;
 
 void main(){
 	krasokorculovanie();
+	pizza_1();
+	pizza_2();
 }
 void krasokorculovanie(){
 	int[] array;
@@ -19,5 +21,42 @@ void krasokorculovanie(){
 	writeln("The score is: "~to!string(sum(array)));
 	return;
 }
-
-//https://github.com/filip12h/Bilikova/blob/master/BONUSOV%C3%89%20%C3%9ALOHY%20-%201.sada%20(for%2C%20if)
+void pizza_1(){
+	int[] array;
+	int result;
+	int input = 1;
+	while(input != 0){
+		input = readln.strip.to!int;
+		if (input!=0){
+			array ~= input;
+		}
+	}
+	if (sum(array)>=20){
+		array = array.remove(minIndex(array));
+	}
+	result = sum(array);
+	writeln("Výsledná suma je: "~to!string(result));
+}
+void pizza_2(){
+	int[] array;
+	int input = 1;
+	float num_1;
+	float num_2;
+	float result;
+	while(input != 0){
+		input = readln.strip.to!int;
+		if (input!=0){
+			array ~= input;
+		}
+	}
+	if (sum(array)>21){
+		num_1 = array[minIndex(array)] / 2;
+		array = array.remove(minIndex(array));
+		num_2 = array[minIndex(array)] / 2;
+		array = array.remove(minIndex(array));
+		result = sum(array) + num_1 + num_2;
+	}else{
+		result = sum(array);
+	}
+	writeln("Výsledná suma je: "~to!string(result));
+}
